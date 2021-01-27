36ecbcd042f6b3e5525958f7968c45ed56a63c4c
Mathieu5419 <mathieuaina@gmail.com>
2021-01-27 09:36:46 +0300

    adding a reservation fonctionality

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -11,6 +11,8 @@ class HousesController < ApplicationController
   # GET /houses/1
   # GET /houses/1.json
   def show 
+    @house = House.find(params[:id])
+    @reservation = @house.reservations.new()
   end
 
   # GET /houses/new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_165104) do
+ActiveRecord::Schema.define(version: 2021_01_26_184133) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,17 @@ ActiveRecord::Schema.define(version: 2021_01_26_165104) do
     t.integer "category_id"
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.datetime "comming_date"
+    t.datetime "leaving_date"
+    t.integer "user_id"
+    t.integer "house_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["house_id"], name: "index_reservations_on_house_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -48,4 +59,6 @@ ActiveRecord::Schema.define(version: 2021_01_26_165104) do
 
   add_foreign_key "houses", "categories"
   add_foreign_key "houses", "users"
+  add_foreign_key "reservations", "houses"
+  add_foreign_key "reservations", "users"
 end
