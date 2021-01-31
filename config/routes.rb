6b47e95c553be1b493dd948d25f6ed8d2b13948c
Nandrian <ranarkai@gmail.com>
2021-01-31 18:56:41 +0300

    create form sign in/up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :account
+  resources :properties
+  devise_for :accounts
   # Set Views/Home/index is the main page
   root 'home#index'
   get 'home/index', to: 'home#index'
