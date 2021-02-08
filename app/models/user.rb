ad8ea7223196161d5d5ba77e6d0c668a2506f4fb
Falitiana <falitianandisa@gmail.com>
2021-02-08 09:28:59 +0300

    modification app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,3 @@ class User < ApplicationRecord
 
   has_many :notifications, foreign_key: :recipient_id
 end
-                                                                                                                                                                                                                                                                                                                                                                                                                                          
